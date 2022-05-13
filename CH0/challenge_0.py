324ea0c8b6a18496cc17e9a2ad1c6a923c24c930
Sebastian Kozłowski <sebastiankozlowski84@gmail.com>
2022-05-13 13:03:16 +0200

    PC-4 Working code, behind tests

--- CH0/challenge_0.py
+++ CH0/challenge_0.py
@@ -6,72 +6,72 @@ pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tessera
 
 url_website = 'http://www.pythonchallenge.com/pc/def/0.html'
 url_image = 'http://www.pythonchallenge.com/pc/def/calc.jpg'
-path = './temp_image.jpg'
+path = './CH0/temp_image.jpg' 
 
 
-class ReadNumberFromImage:
-    
+class ImageNumberReader:   
 
-    def image_download(self):
-        """Download image from url and save it in path""" 
+    def __init__(self, url_image, path):
+        self.url_image = url_image
+        self.path = path
 
-        request.urlretrieve(url_image, path)
-        image = cv2.imread(path)
-        cv2.imshow('Image to read', image)
+    def read_number_from_url(self):
+        self.image_download(url_image)
+        self.image_convert()
+        self.read_number() 
+        return self.read_number
         
-    def image_convert(self):
-        """Converts image to readable text"""
-        #-- Image crop
-        image = cv2.imread(path) 
-        image_crop = image[150:265, 280:440]
-        cv2.imwrite(path, image_crop)
-        cv2.imshow('Image crop', image_crop)
+    def image_download(self, url_image):
+        request.urlretrieve(url_image, path)
 
-        #-- Image grayscale
-        image_crop = cv2.imread(path)
-        image_grayscale = cv2.cvtColor(image_crop, cv2.COLOR_BGR2GRAY)
-        cv2.imshow('Image grayscale', image_grayscale)
+    def image_convert(self):        
+        self.__image_crop()
+        self.__image_grayscale()
+        self.__image_treshold()
+    
+    def __image_crop(self):
+        self.image_crop = cv2.imread(path) 
+        self.image_crop = self.image_crop[150:265, 280:440]
+        cv2.imwrite(path, self.image_crop)
+        #cv2.imshow('Image crop', image_crop)
 
-        #-- Image treshold
-        image_treshold = cv2.adaptiveThreshold(image_grayscale, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
-        cv2.THRESH_BINARY, 111, 30)
-        cv2.imwrite(path, image_treshold) 
-        cv2.imshow('Image treshold', image_treshold)
+    def __image_grayscale(self):       
+        self.image_grayscale = cv2.imread(path)
+        self.image_grayscale = cv2.cvtColor(self.image_crop, cv2.COLOR_BGR2GRAY)
+        cv2.imwrite(path, self.image_grayscale)
+        #cv2.imshow('Image grayscale', image_grayscale)
 
+    def __image_treshold(self):
+        self.image_treshold = cv2.adaptiveThreshold(image_number_reader.image_grayscale, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 111, 30)
+        cv2.imwrite(path, self.image_treshold) 
+        #cv2.imshow('Image treshold', image_treshold)
         #-- Uncomment below line to see conversion results
         #cv2.waitKey(0)
-        cv2.destroyAllWindows()
+        #cv2.destroyAllWindows()
 
-    def image_read_number(self):
-        """Reads text (numbers) from converted image and putt it in variable"""
+    def read_number(self):        
+        self.number = pytesseract.image_to_string(path)
+        return int(self.number)
 
-        number_read = pytesseract.image_to_string(path)
-        return int(number_read)
+class ExponentiationLinkGenerator():
 
-
-    def number_exponentiation(self):
-        """Exponentiation number from image"""
-
-        base_number = 2
-        power_number = ReadNumberFromImage().image_read_number()
-        exponentiation_result = base_number ** power_number
-        return exponentiation_result
+    def __init__(self, number):
+        self.number = number
         
+    def number_exponentiation(self, number):
+        self.number = number
+        self.exponentiation_result = pow(2, number)
+        self.exponentiation_result = int(self.exponentiation_result)
+        print(self.exponentiation_result)                
 
-    def create_new_url(self):
-        """Change url using replace method"""
-        
-        url_to_convert = url_website
-        number = ReadNumberFromImage().number_exponentiation()
-        new_url = url_to_convert.replace('0', str(number))
-        return new_url
+    def create_new_url(self, url_to_convert, exponentiation_result):
+        self.url_to_convert = url_to_convert
+        self.exponentiation_result = exponentiation_result
+        self.new_url = self.url_to_convert.replace('0', str(self.exponentiation_result))
+        print(self.new_url)
 
-read_class = ReadNumberFromImage()
-read_class.image_download()
-read_class.image_convert()
-read_class.image_read_number()
-print(read_class.image_read_number())
-read_class.number_exponentiation()
-print(read_class.number_exponentiation())
-read_class.create_new_url()
-print(read_class.create_new_url())
+image_number_reader = ImageNumberReader(url_image, path)
+image_number_reader.read_number_from_url()
+exponentiation_link_generator = ExponentiationLinkGenerator(image_number_reader.number)
+exponentiation_link_generator.number_exponentiation(image_number_reader.read_number())
+exponentiation_link_generator.create_new_url(url_website, exponentiation_link_generator.exponentiation_result)
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Challenge
+# Python Challenge
+
+I am trying my own abilities to solve Python challenges. I treat these challenges as a way to learn python and to increase the level of my current knowledge. My goal is not only to solve puzzles, but as much as possible to automate getting to the next challenge.
