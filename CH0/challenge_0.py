7bc4ffaf82a513f4e70d325a6f49aa096cf91fbb
Sebastian Kozłowski <sebastiankozlowski84@gmail.com>
2022-06-02 21:41:24 +0200

    PC-10 Add beautiful soup library to find img url.

--- CH0/challenge_0.py
+++ CH0/challenge_0.py
@@ -1,15 +1,21 @@
 import cv2
-from numpy import number
 import pytesseract
 from urllib import request
-import requests
 import os
+from bs4 import BeautifulSoup
+import requests
+
+
+url_website = 'http://www.pythonchallenge.com/pc/def/0.html'
+r = requests.get(url_website)
+soup = BeautifulSoup(r.content, 'html.parser')
+img_link = soup.find('img').attrs
 
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
-url_website = 'http://www.pythonchallenge.com/pc/def/0.html'
 url_image = 'http://www.pythonchallenge.com/pc/def/calc.jpg'
 path = 'temp_image.jpg' 
+
 
 class ImageNumberReader:   
     
@@ -22,20 +28,22 @@ class ImageNumberReader:
         self.path = path
 
     def download_image_from_url_to_path(self):
-        """ Download image from url to path"""
-
+        """ Download image from url to path """
         request.urlretrieve(url_image, path)
 
     def image_convert_to_read_number(self): 
-        """ Convert image to be readable by tersseract."""
-
-        self.__image_croping(150, 260, 265, 440)
-        self.__image_thresholding(255, 121)
+        """ Convert image to be readable by tersseract """
+        self.__image_croping()
+        self.__image_thresholding()
 
              
     def __image_croping(self, start_x= 150, end_x= 260, start_y= 265,  end_y= 440 ):
-        """ Cropping image to specific dimension. """
-
+        """ Cropp image to specific dimension. 
+        start_x  --- starting pixel on x axis
+        end_x --- last pixel on x axis
+        start_y  --- starting pixel on x axis
+        end_y --- last pixel on x axis
+        """
         image_to_crop = cv2.imread(path)
         img_height = image_to_crop.shape[0]
         img_width = image_to_crop.shape[1]
@@ -55,68 +63,77 @@ class ImageNumberReader:
         cv2.imwrite(path, image_to_crop)
 
 
-    def __image_thresholding(self, white_level = 255, block_size = 111):
-        """ Changing image to 8-bit grayscale an add threshold."""
-
-        assert white_level <= 255, f'White level must be between 0-255'
-        assert white_level >= 0, f'White level must be between 0-255'
+    def __image_thresholding(self, max_pixel_value = 255, block_size = 111, constant = 30):
+        """ Change image to 8-bit grayscale and add threshold. 
+        max_pixel_value --- it specifies the maximum value which is assigned to pixel values exceeding the threshold.
+        block_size --- it is the size of a pixel neighbourhood that is used to calculate a threshold value.
+        constant --- a constant value that is subtracted from the mean or weighted sum of the neighbourhood pixels.
+            
+        """
+        assert max_pixel_value <= 255, f'Max pixel value must be between 0-255'
+        assert max_pixel_value >= 0, f'Max pixel value must be between 0-255'
         assert block_size <= 255, f'Pixel block must be odd number between 0-255'
         assert block_size % 2 > 0 , f'Pixel block must be odd number'
 
-        self.white_level = white_level
+        self.max_pixel_value = max_pixel_value
         self.block_size = block_size
 
         image_to_threshold = cv2.imread(path, 0) # 0 parameter change image to grayscale image
-        image_to_threshold = cv2.adaptiveThreshold(image_to_threshold, white_level, 
-        cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, block_size, 30) # 111, 30
+        image_to_threshold = cv2.adaptiveThreshold(image_to_threshold, max_pixel_value, 
+        cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, block_size, constant) 
         cv2.imwrite(path, image_to_threshold) 
-        cv2.imshow('Image treshold', image_to_threshold)
-        cv2.waitKey(0)
-        #cv2.destroyAllWindows()
 
-    def read_number(self):        
-        number = pytesseract.image_to_string(path)
-        print(number)
-        return int(number)
+    def read_number_from_image(self):  
+        """ Reads text from image and returns to variable """
+
+        global read_number
+        read_number = pytesseract.image_to_string(path)
+        read_number = int(read_number)
+        print(read_number)
 
 
-# class ExponentiationLinkGenerator():
-
-#     def __init__(self, number):
-#         self.number = number
+class ExponentiationLinkGenerator():
+    """ Exponentiation operation and creat new url adres """
+    def __init__(self, base_number, pow_number):
+        self.base_number = base_number
+        self.pow_number = pow_number
         
-#     def number_exponentiation(self, number):
-#         self.number = number
-#         self.exponentiation_result = pow(2, number)
-#         self.exponentiation_result = int(self.exponentiation_result)
-#         return self.exponentiation_result
-#         # print(self.exponentiation_result)                
-
-#     def create_new_url(self, url_to_convert, exponentiation_result):
-#         self.url_to_convert = url_to_convert
-#         self.exponentiation_result = exponentiation_result
-#         self.new_url = self.url_to_convert.replace('0', str(self.exponentiation_result))
-#         self.new_url = str(self.new_url)
-#         print(self.new_url)
+    def number_exponentiation(self):
+        """ Number exponentiation """
+        global exponentiation_result
+        exponentiation_result = pow(self.pow_number, self.base_number)
+        print(exponentiation_result) 
         
-#     def open_web_browser_new_url(self, url):
-#         self.url = url
-#         url = str(self.new_url)
-#         os.system('start chrome ' + self.new_url)
+
+    def create_new_url(self, url_to_convert, exponentiation_result):
+        """ Create new url adres """
+        self.url_to_convert = url_to_convert
+        self.exponentiation_result = exponentiation_result
+        global new_url
+        new_url = self.url_to_convert.replace('0', str(exponentiation_result))
+        new_url = str(new_url)
+        print(new_url)
+        return new_url
+        
+        
+    def open_web_browser_new_url(self, new_url):
+        """ Open chrome browser with generated link and delete path file (temp_image) """
+        self.new_url = new_url
+        os.system('start chrome ' + self.new_url)
+        os.remove(path)
 
 def main():
-
+    """ Main function"""
     image_number_reader = ImageNumberReader(url_image, path)
     image_number_reader.download_image_from_url_to_path()
     image_number_reader.image_convert_to_read_number()
-    image_number_reader.read_number()
-    
+    image_number_reader.read_number_from_image()
+    exponentiation_link_generator = ExponentiationLinkGenerator(read_number, 2)
+    exponentiation_link_generator.number_exponentiation()
+    exponentiation_link_generator.create_new_url(url_website, exponentiation_result)
+    exponentiation_link_generator.open_web_browser_new_url(new_url)
 
 if __name__ == "__main__":
     main()
 
-# image_number_reader.read_number_from_url()
-# exponentiation_link_generator = ExponentiationLinkGenerator(image_number_reader.number)
-# exponentiation_link_generator.number_exponentiation(image_number_reader.read_number())
-# exponentiation_link_generator.create_new_url(url_website, exponentiation_link_generator.exponentiation_result)
-# exponentiation_link_generator.open_web_browser_new_url(ExponentiationLinkGenerator.create_new_url)
+
